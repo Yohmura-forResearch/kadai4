d77c5b2cead5415df1533572c3b7f84c7a8ec802
Yohmura for test <you@example.com>
2025-06-06 15:54:23 +0000

    最大公約数を計算するシェルスクリプト

---
+++ saidaikouyakusu.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#最大公約数を求める関数
+saidaikouyakusu() {
+	local a=$1
+	local b=$2
+
+	while [[$b -ne 0 ]];do
+		local temp=$b
+		b=$((a % b))
+		a=$temp
+	done
+
+	echo $a
+}
+
+# 引数のチェック
+if [[ $# -ne 2 ]]; then
+	echo "Usage: $0 <num1> <num2>"
+	exit 1
+fi
+
+# 計算実行
+saidaikouyakusu "$1" "$2"
